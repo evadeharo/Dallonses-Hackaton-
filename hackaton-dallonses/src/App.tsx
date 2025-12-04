000798b9c284e29c26b20dbe36d5c5fa3b244bbf
Ruben Montagut <ruben.pinell01@gmail.com>
2025-12-04 15:04:13 +0100

    part1

--- hackaton-dallonses/src/App.tsx
+++ hackaton-dallonses/src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import data from "./data.json";
 
 export default function App() {
+  const { part1, part2, part3, part4 } = data;
   return (
     <main style={{ display: "flex", flexWrap: "wrap" }}>
       <div
@@ -11,6 +13,23 @@ export default function App() {
           position: "relative",
         }}
       >
+        {part1.map((p, i) => (
+          <div
+            key={i}
+            style={{
+              position: "absolute",
+              left: `${p.x * 100}%`,
+              top: `${p.y * 100}%`,
+              width: p.size,
+              height: p.size,
+              backgroundColor: "black",
+              borderRadius: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            {""}
+          </div>
+        ))}
         <span style={{ position: "absolute", left: "0", bottom: "0" }}>0</span>
         <span style={{ position: "absolute", left: "95%", bottom: "95%" }}>
           100
