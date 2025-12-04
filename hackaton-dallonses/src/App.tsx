9017f2713b50b7f6d099278044961cf6dfb3658d
Ruben Montagut <ruben.pinell01@gmail.com>
2025-12-04 14:37:08 +0100

    logic

--- hackaton-dallonses/src/App.tsx
+++ hackaton-dallonses/src/App.tsx
@@ -1,32 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 export default function App() {
   const [firstNumber, setFirstNumber] = useState<number | undefined>(undefined);
+  const [firstNumberLoading, setFirstNumberLoading] = useState<boolean>(true);
+  const [secondNumberLoading, setSecondNumberLoading] =
+    useState<boolean>(false);
   const [secondNumber, setSecondNumber] = useState<number | undefined>(
     undefined
   );
   const [percentage, setPercentage] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const getRandomInt = (max: number) => {
     return Math.floor(Math.random() * max);
   };
 
-  const startRoulette = () => {
-    if (firstNumber === undefined) {
-      setFirstNumber(getRandomInt(9));
-    } else if (firstNumber !== undefined && secondNumber === undefined) {
-      setSecondNumber(getRandomInt(9));
+  useEffect(() => {
+    while (firstNumberLoading) {
+      setTimeout(() => {
+        setFirstNumber(getRandomInt(9));
+      }, 300);
     }
-    if (firstNumber !== undefined && secondNumber !== undefined) {
-      const percentageVal = getRandomInt(10);
-      if (percentageVal > 8) {
+  }, [firstNumberLoading]);
+
+  useEffect(() => {
+    while (secondNumberLoading) {
+      setTimeout(() => {
+        setSecondNumber(getRandomInt(9));
+      }, 300);
+    }
+  }, [secondNumberLoading]);
+
+  const startRoulette = () => {
+    if (firstNumberLoading) {
+      setFirstNumberLoading(false);
+      setSecondNumberLoading(true);
+      return;
+    }
+    if (secondNumberLoading) {
+      setSecondNumberLoading(false);
+      setFirstNumberLoading(false);
+      return;
+    }
+
+    if (!secondNumberLoading && !firstNumberLoading) {
+      const randomVal = getRandomInt(10);
+      if (randomVal > 8) {
         setPercentage(true);
       } else {
         setPercentage(false);
-        setFirstNumber(undefined);
-        setSecondNumber(undefined);
       }
+      setFirstNumberLoading(true);
+      setSecondNumberLoading(false);
     }
   };
 
