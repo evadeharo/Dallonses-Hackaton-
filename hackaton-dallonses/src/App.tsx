888f55d8f4574f3fa1c79b8294b6c9d6287bd2a8
evadeharomunoz <eva@dallonses.com>
2025-12-04 14:57:45 +0100

    fix: grid

--- hackaton-dallonses/src/App.tsx
+++ hackaton-dallonses/src/App.tsx
@@ -3,10 +3,10 @@ import "./App.css";
 export default function App() {
   return (
     <main style={{ display: "flex", flexWrap: "wrap" }}>
-      <div style={{ width: "50vw", height: "50vh" }}>part 1</div>
-      <div style={{ width: "50vw", height: "50vh" }}>part 2</div>
-      <div style={{ width: "50vw", height: "50vh" }}>part 3</div>
-      <div style={{ width: "50vw", height: "50vh" }}>part 4</div>
+      <div style={{ width: "40vw", height: "50vh", backgroundColor: "red" }}>part 1</div>
+      <div style={{ width: "40vw", height: "50vh", backgroundColor: "peru" }}>part 2</div>
+      <div style={{ width: "40vw", height: "50vh", backgroundColor: "blue" }}>part 3</div>
+      <div style={{ width: "40vw", height: "50vh", backgroundColor: "yellow" }}>part 4</div>
     </main>
   );
 }
