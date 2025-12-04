41029e6fb0b3beb92cbd0144a3fc6bbb0b275418
EmyDallonses <81176813+EmyDallonses@users.noreply.github.com>
2025-12-04 14:25:19 +0100

    whatever

--- hackaton-dallonses/src/App.tsx
+++ hackaton-dallonses/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const audio = document.querySelector("audio");
+  if (!audio) return;
+
+  audio.volume = 0.9;
+  audio.controls = false;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ width: "100%", height: "100%" }}>
+      <audio controls>
+        <source
+          src="https://media.dallonses.com/hackaton/dimoni.mp3"
+          type="audio/mpeg"
+        />
+      </audio>
+      <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
+        <div style={{ width: 100, height: 200, backgroundColor: "red" }} />
+        <div style={{ width: 100, height: 200, backgroundColor: "blue" }} />
+        <div style={{ width: 100, height: 200, backgroundColor: "green" }} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
