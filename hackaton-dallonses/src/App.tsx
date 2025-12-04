0460cdad68a9e991d8962fa59236433ca3633378
evadeharomunoz <eva@dallonses.com>
2025-12-04 15:01:00 +0100

    fix: layout

--- hackaton-dallonses/src/App.tsx
+++ hackaton-dallonses/src/App.tsx
@@ -3,10 +3,61 @@ import "./App.css";
 export default function App() {
   return (
     <main style={{ display: "flex", flexWrap: "wrap" }}>
-      <div style={{ width: "40vw", height: "50vh", backgroundColor: "red" }}>part 1</div>
-      <div style={{ width: "40vw", height: "50vh", backgroundColor: "peru" }}>part 2</div>
-      <div style={{ width: "40vw", height: "50vh", backgroundColor: "blue" }}>part 3</div>
-      <div style={{ width: "40vw", height: "50vh", backgroundColor: "yellow" }}>part 4</div>
+      <div
+        style={{
+          width: "40vw",
+          height: "45vh",
+          backgroundColor: "red",
+          position: "relative",
+        }}
+      >
+        <span style={{ position: "absolute", left: "0", bottom: "0" }}>0</span>
+        <span style={{ position: "absolute", left: "95%", bottom: "95%" }}>
+          100
+        </span>
+      </div>
+      <div
+        style={{
+          width: "40vw",
+          height: "45vh",
+          backgroundColor: "peru",
+          position: "relative",
+        }}
+      >
+        part 2
+        <span style={{ position: "absolute", left: "0", bottom: "0" }}>0</span>
+        <span style={{ position: "absolute", left: "95%", bottom: "95%" }}>
+          100
+        </span>
+      </div>
+      <div
+        style={{
+          width: "40vw",
+          height: "45vh",
+          backgroundColor: "blue",
+          position: "relative",
+        }}
+      >
+        part 3
+        <span style={{ position: "absolute", left: "0", bottom: "0" }}>0</span>
+        <span style={{ position: "absolute", left: "95%", bottom: "95%" }}>
+          100
+        </span>
+      </div>
+      <div
+        style={{
+          width: "40vw",
+          height: "45vh",
+          backgroundColor: "yellow",
+          position: "relative",
+        }}
+      >
+        part 4
+        <span style={{ position: "absolute", left: "0", bottom: "0" }}>0</span>
+        <span style={{ position: "absolute", left: "95%", bottom: "95%" }}>
+          100
+        </span>
+      </div>
     </main>
   );
 }
