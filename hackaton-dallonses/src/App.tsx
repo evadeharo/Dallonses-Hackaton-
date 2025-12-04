3e4bcfe9fcdda8bc7229b33a8b66ecbd6ba17e04
Ruben Montagut <ruben.pinell01@gmail.com>
2025-12-04 14:22:06 +0100

    add logic for 3 numbers

--- hackaton-dallonses/src/App.tsx
+++ hackaton-dallonses/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [firstNumber, setFirstNumber] = useState<number | undefined>(undefined);
+  const [secondNumber, setSecondNumber] = useState<number | undefined>(
+    undefined
+  );
+  const [percentage, setPercentage] = useState<boolean>(false);
+
+  const getRandomInt = (max: number) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  const startRoulette = () => {
+    if (firstNumber === undefined) {
+      setFirstNumber(getRandomInt(9));
+    } else if (firstNumber !== undefined && secondNumber === undefined) {
+      setSecondNumber(getRandomInt(9));
+    }
+    if (firstNumber !== undefined && secondNumber !== undefined) {
+      const percentageVal = getRandomInt(10);
+      if (percentageVal > 8) {
+        setPercentage(true);
+      } else {
+        setPercentage(false);
+        setFirstNumber(undefined);
+        setSecondNumber(undefined);
+      }
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>{firstNumber}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={startRoulette}>Random number</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {firstNumber ?? 0} - {secondNumber ?? 0} {percentage ? "%" : ""}
       </p>
     </>
-  )
+  );
 }
-
-export default App
