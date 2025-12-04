88512a05dfaa262652bd2e670c765aaf86754738
evadeharomunoz <eva@dallonses.com>
2025-12-04 14:56:46 +0100

    test

--- hackaton-dallonses/src/App.tsx
+++ hackaton-dallonses/src/App.tsx
@@ -2,8 +2,11 @@ import "./App.css";
 
 export default function App() {
   return (
-    <main>
-      hola
+    <main style={{ display: "flex", flexWrap: "wrap" }}>
+      <div style={{ width: "50vw", height: "50vh" }}>part 1</div>
+      <div style={{ width: "50vw", height: "50vh" }}>part 2</div>
+      <div style={{ width: "50vw", height: "50vh" }}>part 3</div>
+      <div style={{ width: "50vw", height: "50vh" }}>part 4</div>
     </main>
   );
 }
